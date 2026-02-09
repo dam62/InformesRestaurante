5a5e78c682908ce524f67b16b73329da64a764fb
dam62 <dam62@gmail.com>
2026-02-09 15:33:30 +0100

    Commit

---
+++ InformesRestaurante/Views/InformesRestaurante/InformeReserva.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace InformesRestaurante.Views.InformesRestaurante;
+
+public partial class InformeReserva : UserControl
+{
+    public InformeReserva()
+    {
+        InitializeComponent();
+    }
+}
