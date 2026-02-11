1951afe026571cc6fe33475aa19897b0740075c1
dam62 <dam62@gmail.com>
2026-02-11 17:09:25 +0100

    Commit

--- InformesRestaurante/ViewModels/MainWindowViewModel.cs
+++ InformesRestaurante/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using InformesRestaurante.Services;
 
@@ -9,11 +10,18 @@ public partial class MainWindowViewModel : ViewModelBase
     private N8NService n8nService = new();
     [ObservableProperty] private string url;
     [ObservableProperty] private bool seMuestraPDF = false;
+    
+    [RelayCommand]
+    public async Task CargarDesplegablesAsync()
+    {
+        
+    }
 
     [RelayCommand]
     public void GenerarPDFReserva(string id)
     {
-        Url = "localhost:8080//" + id;
+        Url = "localhost:10000/eriRestaurant/informeReserva/" + id;
+        SeMuestraPDF = true;
     }
     
     [RelayCommand]
