db4351cb9c970ce46b082db28e193cdc8a34b32e
dam62 <dam62@gmail.com>
2026-02-23 18:33:36 +0100

    Commit

---
+++ InformesRestaurante/Models/Empleado.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace InformesRestaurante.Models;
+
+public class Empleado
+{
+    [JsonProperty("id_empleado", DefaultValueHandling = DefaultValueHandling.Ignore)]
+    public int Id { get; set; }
+    
+    [JsonProperty("nombre")]
+    public string Nombre { get; set; }
+
+    public override string ToString()
+    {
+        return Nombre;
+    }
+}
